314f5e94e99573f78c5ba01d074e115a307e4b50
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2023-02-11 15:55:18 -0600

    changed to declared methods

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/ProphetPlugin.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/ProphetPlugin.java
@@ -168,6 +168,7 @@ public class ProphetPlugin {
 
     private void processService(Class<?> clazz) {
 
+
         java.lang.reflect.Field[] fields = clazz.getDeclaredFields();
 
         System.out.println("===== Fields: " + clazz.getName() + " =====\n");
@@ -178,7 +179,7 @@ public class ProphetPlugin {
         }
         System.out.println("\n===== END Fields =====\n");
 
-        Method[] m = clazz.getMethods();
+        Method[] m = clazz.getDeclaredMethods();
 
         System.out.println("===== Methods: " + clazz.getName() + " =====\n");
         for (Method meth : m) {
